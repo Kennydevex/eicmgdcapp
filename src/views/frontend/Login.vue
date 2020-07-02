3b3a2571292a3ab267d4774140776119f3ec4870
Kenny <kenny.devex@gmail.com>
2020-07-02 13:25:53 -0100

    Auth form optimization

--- src/views/frontend/Login.vue
+++ src/views/frontend/Login.vue
@@ -4,90 +4,91 @@
     <!-- <v-container fluid fill-height> -->
     <!-- ../../assets/app/statics/login_cover.gif -->
 
-    <v-row justify="center" align="center">
-      <v-col align="center" cols="12" sm="8" md="6" lg="4">
-        <v-card class="elevation-10" width="350">
-          <v-card-text class="mb-0 pb-0">
-            <div class="layout column align-center">
-              <v-avatar color="primary" size="62">
-                <v-icon large dark>mdi-account-lock</v-icon>
-              </v-avatar>
-              <h1 class="flex my-4 primary--text font-weight-light">
-                EICM -
-                <span class="font-weight-thin">Login</span>
-              </h1>
-            </div>
+    <v-container grid-list-xs>
+      <v-row justify="center" align="center">
+        <v-col align="center" cols="12" sm="8" md="6" lg="4">
+          <v-card class="elevation-10" width="350">
+            <v-card-text class="mb-0 pb-0">
+              <div class="layout column align-center">
+                <v-avatar color="primary" size="62">
+                  <v-icon large dark>mdi-account-lock</v-icon>
+                </v-avatar>
+                <h1 class="flex my-4 primary--text font-weight-light">
+                  EICM -
+                  <span class="font-weight-thin">Login</span>
+                </h1>
+              </div>
 
-            <v-flex xs12>
-              <v-alert
-                dense
-                tile
-                :value="loginError"
-                color="error"
-                icon="mdi-lock-alert"
-                border="right"
-                colored-border
-                dismissible
-                elevation="3"
-                transition="scale-transition"
-              >
-                <small>{{loginError}}</small>
-              </v-alert>
-            </v-flex>
+              <v-flex xs12>
+                <v-alert
+                  dense
+                  tile
+                  :value="loginError"
+                  color="error"
+                  icon="mdi-lock-alert"
+                  border="right"
+                  colored-border
+                  dismissible
+                  elevation="3"
+                  transition="scale-transition"
+                >
+                  <small>{{loginError}}</small>
+                </v-alert>
+              </v-flex>
 
-            <v-form>
-              <v-container grid-list-sm ma-0 pa-0>
-                <v-row>
-                  <v-col cols="12">
-                    <v-text-field
-                      dense
-                      outlined
-                      v-model="formData.email"
-                      name="login"
-                      label="Email"
-                      type="text"
-                      @keyup.enter="authenticate()"
-                      v-validate="'required|email'"
-                      data-vv-name="email"
-                      :error-messages="errors.collect('email')"
-                    ></v-text-field>
-                  </v-col>
-                </v-row>
-                <v-row>
-                  <v-col cols="12" class="py-0 ma-y">
-                    <v-text-field
-                      dense
-                      outlined
-                      v-model="formData.password"
-                      :append-icon="passIcon ? 'mdi-eye' : 'mdi-eye-off'"
-                      @click:append="() => (passIcon = !passIcon)"
-                      :type="passIcon ? 'password' : 'text'"
-                      name="password"
-                      label="Password"
-                      @keyup.enter="authenticate()"
-                      v-validate="'required'"
-                      data-vv-name="password"
-                      :error-messages="errors.collect('password')"
-                    ></v-text-field>
-                  </v-col>
-                </v-row>
-              </v-container>
-            </v-form>
-          </v-card-text>
-          <v-card-actions class="mt-0 pt-0">
-            <v-row>
-              <v-col cols="12" class="py-0 my-0">
-                <v-btn
-                  class="text-none font-weight-light no"
-                  :to="{name: 'register_page'}"
-                  small
-                  text
-                  dark
-                  color="primary"
-                >criar uma conta</v-btn>
-              </v-col>
+              <v-form>
+                <v-container grid-list-sm ma-0 pa-0>
+                  <v-row>
+                    <v-col cols="12">
+                      <v-text-field
+                        dense
+                        outlined
+                        v-model="formData.email"
+                        name="login"
+                        label="Email"
+                        type="text"
+                        @keyup.enter="authenticate()"
+                        v-validate="'required|email'"
+                        data-vv-name="email"
+                        :error-messages="errors.collect('email')"
+                      ></v-text-field>
+                    </v-col>
+                  </v-row>
+                  <v-row>
+                    <v-col cols="12" class="py-0 ma-y">
+                      <v-text-field
+                        dense
+                        outlined
+                        v-model="formData.password"
+                        :append-icon="passIcon ? 'mdi-eye' : 'mdi-eye-off'"
+                        @click:append="() => (passIcon = !passIcon)"
+                        :type="passIcon ? 'password' : 'text'"
+                        name="password"
+                        label="Password"
+                        @keyup.enter="authenticate()"
+                        v-validate="'required'"
+                        data-vv-name="password"
+                        :error-messages="errors.collect('password')"
+                      ></v-text-field>
+                    </v-col>
+                  </v-row>
+                </v-container>
+              </v-form>
+            </v-card-text>
+            <v-card-actions class="mt-0 pt-0">
+              <v-row>
+                <v-col cols="12" class="py-0 my-0">
+                  <v-btn
+                    class="text-none font-weight-light no"
+                    :to="{name: 'register_page'}"
+                    small
+                    text
+                    dark
+                    color="primary"
+                  >criar uma conta</v-btn>
+                </v-col>
 
-              <!-- <v-col>
+                <!-- <v-col>
                     <small>Autenticar usando a minha conta de:</small>
                   </v-col>
 
@@ -101,16 +102,18 @@
                     <v-btn x-small fab dark color="google">
                       <v-icon dark>mdi-google-plus</v-icon>
                     </v-btn>
-              </v-col>-->
+                </v-col>-->
 
-              <v-col cols="12" align="center">
-                <v-btn @click="authenticate()" tile color="primary" class="text-none">Autenticar</v-btn>
-              </v-col>
-            </v-row>
-          </v-card-actions>
-        </v-card>
-      </v-col>
-    </v-row>
+                <v-col cols="12" align="center">
+                  <v-btn @click="authenticate()" tile color="primary" class="text-none">Autenticar</v-btn>
+                </v-col>
+              </v-row>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+
     <!-- </v-container> -->
     <!-- </v-app> -->
   </div>
--- src/views/frontend/Register.vue
+++ src/views/frontend/Register.vue
@@ -1,99 +1,100 @@
 <template>
   <div class="background-login">
-    <v-row align="center" justify="center">
-      <v-col align="center" cols="12" sm="8" md="6" lg="4">
-        <v-card class="elevation-10 pa-3" width="500">
-          <v-card-text class="mb-0 pb-0">
-            <div class="layout column align-center">
-              <v-avatar color="primary" size="62">
-                <v-icon large dark>mdi-account-plus</v-icon>
-              </v-avatar>
-              <h1 class="flex my-4 primary--text font-weight-light">
-                EICM -
-                <span class="font-weight-thin">Regoistar</span>
-              </h1>
-            </div>
-            <v-form ref="form">
-              <v-container grid-list-md ma-0 pa-0>
-                <v-row>
-                  <v-col cols="12" class="my-0 py-0">
-                    <v-alert type="error" outlined :value="hasError">
-                      <small>Erros resultantes do processamento, corrija-os e re-envie</small>
-                    </v-alert>
-                  </v-col>
-                </v-row>
-                <v-row>
-                  <v-col cols="12" md="6" class="py-0 my-0">
-                    <v-text-field
-                      outlined
-                      dense
-                      v-model="formData.folk.name"
-                      name="name"
-                      label="Nome*"
-                      type="text"
-                      v-validate="'required|alpha'"
-                      data-vv-name="name"
-                      :error-messages="errors.collect('name')"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col cols="12" md="6" class="py-0 my-0">
-                    <v-text-field
-                      outlined
-                      dense
-                      v-model="formData.folk.lastname"
-                      name="lastname"
-                      label="Apelidos*"
-                      type="text"
-                      v-validate="'required|alpha_spaces'"
-                      data-vv-name="lastname"
-                      :error-messages="errors.collect('lastname')"
-                    ></v-text-field>
-                  </v-col>
+    <v-container grid-list-xs>
+      <v-row align="center" justify="center">
+        <v-col align="center" cols="12" sm="8" md="6" lg="4">
+          <v-card class="elevation-10 pa-3" width="500">
+            <v-card-text class="mb-0 pb-0">
+              <div class="layout column align-center">
+                <v-avatar color="primary" size="62">
+                  <v-icon large dark>mdi-account-plus</v-icon>
+                </v-avatar>
+                <h1 class="flex my-4 primary--text font-weight-light">
+                  EICM -
+                  <span class="font-weight-thin">Regoistar</span>
+                </h1>
+              </div>
+              <v-form ref="form">
+                <v-container grid-list-md ma-0 pa-0>
+                  <v-row>
+                    <v-col cols="12" class="my-0 py-0">
+                      <v-alert type="error" outlined :value="hasError">
+                        <small>Erros resultantes do processamento, corrija-os e re-envie</small>
+                      </v-alert>
+                    </v-col>
+                  </v-row>
+                  <v-row>
+                    <v-col cols="12" md="6" class="py-0 my-0">
+                      <v-text-field
+                        outlined
+                        dense
+                        v-model="formData.folk.name"
+                        name="name"
+                        label="Nome*"
+                        type="text"
+                        v-validate="'required|alpha'"
+                        data-vv-name="name"
+                        :error-messages="errors.collect('name')"
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="12" md="6" class="py-0 my-0">
+                      <v-text-field
+                        outlined
+                        dense
+                        v-model="formData.folk.lastname"
+                        name="lastname"
+                        label="Apelidos*"
+                        type="text"
+                        v-validate="'required|alpha_spaces'"
+                        data-vv-name="lastname"
+                        :error-messages="errors.collect('lastname')"
+                      ></v-text-field>
+                    </v-col>
 
-                  <v-col cols="12" class="py-0 my-0">
-                    <v-text-field
-                      outlined
-                      dense
-                      v-model="formData.email"
-                      name="email"
-                      label="Correio Eletrónico*"
-                      type="text"
-                      hint="exemplo@exemplo.cv"
-                      v-validate="'required|email'"
-                      data-vv-name="email"
-                      :error-messages="errorMsg('email') || errors.collect('email')"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col cols="12" md="6" class="py-0 my-0">
-                    <v-text-field
-                      outlined
-                      dense
-                      v-model="formData.username"
-                      name="username"
-                      label="Nome de Utilizador*"
-                      type="text"
-                      v-validate="'required|alpha_dash'"
-                      data-vv-name="username"
-                      :error-messages="errorMsg('username') || errors.collect('username')"
-                    ></v-text-field>
-                  </v-col>
+                    <v-col cols="12" class="py-0 my-0">
+                      <v-text-field
+                        outlined
+                        dense
+                        v-model="formData.email"
+                        name="email"
+                        label="Correio Eletrónico*"
+                        type="text"
+                        hint="exemplo@exemplo.cv"
+                        v-validate="'required|email'"
+                        data-vv-name="email"
+                        :error-messages="errorMsg('email') || errors.collect('email')"
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="12" md="6" class="py-0 my-0">
+                      <v-text-field
+                        outlined
+                        dense
+                        v-model="formData.username"
+                        name="username"
+                        label="Nome de Utilizador*"
+                        type="text"
+                        v-validate="'required|alpha_dash'"
+                        data-vv-name="username"
+                        :error-messages="errorMsg('username') || errors.collect('username')"
+                      ></v-text-field>
+                    </v-col>
 
-                  <v-col cols="12" md="6" class="py-0 my-0">
-                    <v-text-field
-                      outlined
-                      dense
-                      v-model="formData.password"
-                      name="password"
-                      label="Palavra Passe*"
-                      type="password"
-                      ref="password"
-                      v-validate="'required|min:8'"
-                      data-vv-name="password"
-                      :error-messages="errors.collect('password')"
-                    ></v-text-field>
-                  </v-col>
+                    <v-col cols="12" md="6" class="py-0 my-0">
+                      <v-text-field
+                        outlined
+                        dense
+                        v-model="formData.password"
+                        name="password"
+                        label="Palavra Passe*"
+                        type="password"
+                        ref="password"
+                        v-validate="'required|min:8'"
+                        data-vv-name="password"
+                        :error-messages="errors.collect('password')"
+                      ></v-text-field>
+                    </v-col>
 
-                  <!-- <v-col cols="12" md="6" class="py-0 my-0">
+                    <!-- <v-col cols="12" md="6" class="py-0 my-0">
                     <v-text-field
                       outlined
                       dense
@@ -105,41 +106,41 @@
                       data-vv-name="password_confirmation"
                       :error-messages="errors.collect('password_confirmation')"
                     ></v-text-field>
-                  </v-col>-->
+                    </v-col>-->
 
-                  <v-col cols="12" align="left" class="my-0 py-0">
-                    <span class="grey--text text--darken-2">Escolha o Sexo (Opcional)</span>
-                  </v-col>
-                  <v-col cols="12" class="py-0 my-0">
-                    <v-radio-group
-                      dense
-                      row
-                      v-model="formData.folk.gender"
-                      v-validate="'included:0,1,2'"
-                      data-vv-name="gender"
-                      :error-messages="errors.collect('gender')"
-                    >
-                      <v-radio label="Masculino" value="0"></v-radio>
-                      <v-radio label="Feminino" value="1"></v-radio>
-                    </v-radio-group>
-                  </v-col>
-                </v-row>
-              </v-container>
-            </v-form>
-          </v-card-text>
-          <v-card-actions class="my-0 py-0">
-            <v-row>
-              <v-col cols="12" class="py-0 my-0">
-                <v-btn
-                  class="text-none font-weight-light"
-                  :to="{name: 'login_page'}"
-                  small
-                  text
-                  dark
-                  color="primary"
-                >Fazer Login em vez disso</v-btn>
-              </v-col>
-              <!-- <v-col>
+                    <v-col cols="12" align="left" class="my-0 py-0">
+                      <span class="grey--text text--darken-2">Escolha o Sexo (Opcional)</span>
+                    </v-col>
+                    <v-col cols="12" class="py-0 my-0">
+                      <v-radio-group
+                        dense
+                        row
+                        v-model="formData.folk.gender"
+                        v-validate="'included:0,1,2'"
+                        data-vv-name="gender"
+                        :error-messages="errors.collect('gender')"
+                      >
+                        <v-radio label="Masculino" value="0"></v-radio>
+                        <v-radio label="Feminino" value="1"></v-radio>
+                      </v-radio-group>
+                    </v-col>
+                  </v-row>
+                </v-container>
+              </v-form>
+            </v-card-text>
+            <v-card-actions class="my-0 py-0">
+              <v-row>
+                <v-col cols="12" class="py-0 my-0">
+                  <v-btn
+                    class="text-none font-weight-light"
+                    :to="{name: 'login_page'}"
+                    small
+                    text
+                    dark
+                    color="primary"
+                  >Fazer Login em vez disso</v-btn>
+                </v-col>
+                <!-- <v-col>
                 <small>Regsitar conta utilizando:</small>
               </v-col>
               <v-col cols="12" class="py-0 my-0">
@@ -152,26 +153,27 @@
                 <v-btn x-small fab dark color="google">
                   <v-icon dark>mdi-google-plus</v-icon>
                 </v-btn>
-              </v-col>-->
-              <v-col cols="12" align="center">
-                <v-btn
-                  tile
-                  class="text-none ma-1"
-                  @click="register()"
-                  width="130"
-                  :color="hasError?'error':'primary'"
-                >{{hasError?'Re-Enviar':'Registar'}}</v-btn>
-                <!-- <v-btn class="text-none ma-1" tile  @click="register()" outlined>Sair</v-btn> -->
-              </v-col>
+                </v-col>-->
+                <v-col cols="12" align="center">
+                  <v-btn
+                    tile
+                    class="text-none ma-1"
+                    @click="register()"
+                    width="130"
+                    :color="hasError?'error':'primary'"
+                  >{{hasError?'Re-Enviar':'Registar'}}</v-btn>
+                  <!-- <v-btn class="text-none ma-1" tile  @click="register()" outlined>Sair</v-btn> -->
+                </v-col>
 
-              <v-col cols="12" align="left">
-                <small class="grey--text text--darken-2">(*) - Não pode faltar</small>
-              </v-col>
-            </v-row>
-          </v-card-actions>
-        </v-card>
-      </v-col>
-    </v-row>
+                <v-col cols="12" align="left">
+                  <small class="grey--text text--darken-2">(*) - Não pode faltar</small>
+                </v-col>
+              </v-row>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
